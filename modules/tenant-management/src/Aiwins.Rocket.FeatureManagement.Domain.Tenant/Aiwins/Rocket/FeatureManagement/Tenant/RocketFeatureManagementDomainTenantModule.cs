6eee68283e04e045c80ce2ab60ec4fed1f654012
鲁军 <1044635791@qq.com>
2020-05-12 12:03:21 +0800

    ..

---
+++ modules/tenant-management/src/Aiwins.Rocket.FeatureManagement.Domain.Tenant/Aiwins/Rocket/FeatureManagement/Tenant/RocketFeatureManagementDomainTenantModule.cs
@@ -0,0 +1,16 @@
+﻿using Aiwins.Rocket.Modularity;
+
+namespace Aiwins.Rocket.FeatureManagement.Tenant {
+    [DependsOn (
+        typeof (RocketFeatureManagementDomainModule)
+    )]
+    public class RocketFeatureManagementDomainTenantModule : RocketModule {
+        public override void ConfigureServices (ServiceConfigurationContext context) {
+            Configure<FeatureManagementOptions> (options => {
+                options.Providers.Add<TenantFeatureManagementProvider> ();
+                options.ProviderPolicies[TenantFeatureValueProvider.ProviderName] = "RocketTenantManagement.Tenants.ManageFeatures";
+            });
+
+        }
+    }
+}
