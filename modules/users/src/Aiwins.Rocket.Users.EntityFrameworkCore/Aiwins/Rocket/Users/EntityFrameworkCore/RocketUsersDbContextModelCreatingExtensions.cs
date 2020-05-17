beaba47884b5d6ee6bc1dd82f0fbb92b64ca7b56
鲁军 <1044635791@qq.com>
2020-05-17 17:33:14 +0800

    hotfix:用户名字段Emai设置为lphoneNumber

--- modules/users/src/Aiwins.Rocket.Users.EntityFrameworkCore/Aiwins/Rocket/Users/EntityFrameworkCore/RocketUsersDbContextModelCreatingExtensions.cs
+++ modules/users/src/Aiwins.Rocket.Users.EntityFrameworkCore/Aiwins/Rocket/Users/EntityFrameworkCore/RocketUsersDbContextModelCreatingExtensions.cs
@@ -9,10 +9,10 @@ namespace Aiwins.Rocket.Users.EntityFrameworkCore {
             b.Property (u => u.UserName).IsRequired ().HasMaxLength (RocketUserConsts.MaxUserNameLength).HasColumnName (nameof (IUser.UserName));
             b.Property (u => u.Name).HasMaxLength (RocketUserConsts.MaxNameLength).HasColumnName (nameof (IUser.Name));
             b.Property (u => u.Surname).HasMaxLength (RocketUserConsts.MaxSurnameLength).HasColumnName (nameof (IUser.Surname));
-            b.Property (u => u.Email).IsRequired ().HasMaxLength (RocketUserConsts.MaxEmailLength).HasColumnName (nameof (IUser.Email));
-            b.Property (u => u.EmailConfirmed).HasDefaultValue (false).HasColumnName (nameof (IUser.EmailConfirmed));
-            b.Property (u => u.PhoneNumber).HasMaxLength (RocketUserConsts.MaxPhoneNumberLength).HasColumnName (nameof (IUser.PhoneNumber));
+            b.Property (u => u.PhoneNumber).IsRequired ().HasMaxLength (RocketUserConsts.MaxPhoneNumberLength).HasColumnName (nameof (IUser.PhoneNumber));
             b.Property (u => u.PhoneNumberConfirmed).HasDefaultValue (false).HasColumnName (nameof (IUser.PhoneNumberConfirmed));
+            b.Property (u => u.Email).HasMaxLength (RocketUserConsts.MaxEmailLength).HasColumnName (nameof (IUser.Email));
+            b.Property (u => u.EmailConfirmed).HasDefaultValue (false).HasColumnName (nameof (IUser.EmailConfirmed));
         }
     }
 }
